b371805915bc37e68d9fa3242c91682a3b1516a0
carloz <zabaletacopa@gmail.com>
2023-03-02 03:07:41 -0500

    Routing for books

--- pages/books/[id].js
+++ pages/books/[id].js
@@ -1,9 +1,38 @@
 import { useRouter } from 'next/router'
+import { fetchBooksFromId } from '../../data/utils.js'
 
 function BookDetail() {
-  const router = useRouter()
+  const { query } = useRouter()
+  const bookId = query.id
+  const book = fetchBooksFromId(bookId)
 
-  return <h1>Book Detail</h1>
+  if (!book) {
+    return <p>Loading...</p>
+  }
+
+  return (
+    <div
+      style={{
+        maxWidth: '20rem',
+        background: 'whitesmoke',
+        padding: '1rem',
+        margin: 'auto',
+      }}
+    >
+      <div
+        key={book.id}
+        style={{
+          padding: '1rem 0',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <h1>{book.name}</h1>
+        <p>{book.description}</p>
+      </div>
+    </div>
+  )
 }
 
 export default BookDetail
--- pages/books/index.js
+++ pages/books/index.js
@@ -1,5 +1,54 @@
+import { books } from '../../data/utils.js'
+import Link from 'next/link'
+
 function index() {
-  return <h1>Index Books</h1>
+  return (
+    <div
+      style={{
+        maxWidth: '20rem',
+        background: 'whitesmoke',
+        padding: '1rem',
+        margin: 'auto',
+      }}
+    >
+      {books.map(book => (
+        <div
+          key={book.id}
+          style={{
+            padding: '1rem 0',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <h1>{book.name}</h1>
+          <p>{book.description}</p>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <Link
+              href={`/books/${book.id}`}
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                background: 'lightblue',
+                width: '70px',
+                height: '30px',
+                borderRadius: '5px',
+                color: 'black',
+              }}
+            >
+              Link
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default index
