1676377232e7a7c32ca3dad452de2159b21380e4
carloz <zabaletacopa@gmail.com>
2023-03-03 03:00:54 -0500

    adding .env file

--- utils/api-utils.js
+++ utils/api-utils.js
@@ -1,14 +1,15 @@
 async function getBooks() {
-  const res = await fetch(
-    'https://next-js-29f86-default-rtdb.firebaseio.com/books.json',
-  )
-  const data = await res.json()
+  const url = process.env.FIREBASE_URL
+  const response = await fetch(url)
+  const data = await response.json()
+
   return data
 }
 
 async function getBookFromId(id) {
   const books = await getBooks()
   const book = books.find(book => book.id === id)
+
   return book
 }
 
